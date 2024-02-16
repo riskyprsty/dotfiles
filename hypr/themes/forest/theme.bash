95a757946d42938034052895b0a9eea37ae3fac7
riskyprsty <66415817+riskyprsty@users.noreply.github.com>
2024-02-16 23:59:31 +0700

    first commit

---
+++ hypr/themes/forest/theme.bash
@@ -0,0 +1,57 @@
+# ------------------------------------------------------------------------------
+# Copyright (C) 2020-2022 Aditya Shakya <adi1090x@gmail.com>
+#
+# Forest Theme
+# ------------------------------------------------------------------------------
+
+# Colors
+background='#25222f'
+foreground='#e6dfcf'
+color0='#263640'
+color1='#ec7875'
+color2='#61c766'
+color3='#fdd835'
+color4='#7e4f7b'
+color5='#ba68c8'
+color6='#e6dfcf'
+color7='#e6dfcf'
+color8='#4a697d'
+color9='#fb8784'
+color10='#70d675'
+color11='#ffe744'
+color12='#51b4ff'
+color13='#c979d7'
+color14='#5cdff0'
+color15='#fdf6e3'
+
+accent='#00BCD4'
+light_value='0.05'
+dark_value='0.30'
+
+# Rofi
+rofi_icon='Papirus-Apps'
+
+# Terminal
+terminal_font_name='JetBrainsMono Nerd Font'
+terminal_font_size='12'
+terminal_opacity='0.9'
+
+# Geany
+geany_colors='adapta.conf'
+geany_font='JetBrains Mono 11'
+
+# Appearance
+gtk_font='Noto Sans 12'
+gtk_theme='Sweet-Dark'
+icon_theme='Qogir-Dark'
+cursor_theme='Pear'
+text_scaling_factor='1.05'
+
+# Dunst
+dunst_width='300'
+dunst_height='80'
+dunst_offset='10x40'
+dunst_origin='top-right'
+dunst_font='Iosevka Custom 10'
+dunst_border='2'
+dunst_separator='2'
---
+++ hypr/themes/melody/theme.bash
@@ -0,0 +1,59 @@
+# ------------------------------------------------------------------------------
+# Copyright (C) 2020-2022 Aditya Shakya <adi1090x@gmail.com>
+#
+# Berry Theme
+# ------------------------------------------------------------------------------
+
+# Colors
+background='#282C2F'
+foreground='#F1E1DD'
+color0='#3A424F'
+color1='#D97E96'
+color2='#F0E1DF'
+color3='#838649'
+color4='#73737D'
+color5='#A36584'
+color6='#D4B7A5'
+color7='#F1E1DD'
+color8='#A99390'
+color9='#D97E96'
+color10='#F0E1DF'
+color11='#838649'
+color12='#73737D'
+color13='#A36584'
+color14='#D4B7A5'
+color15='#B5BCC9'
+
+accent='#f5addd'
+element_bg='#EEFFFF'
+element_fg='#303030'
+
+light_value='0.64'
+dark_value='0.08'
+
+# Rofi
+rofi_icon='Zafiro'
+
+# Terminal
+terminal_font_name='JetBrainsMono Nerd Font'
+terminal_font_size='12'
+terminal_opacity='0.9'
+
+# Geany
+geany_colors='arc.conf'
+geany_font='JetBrains Mono 11'
+
+# Appearance
+gtk_font='Noto Sans 12'
+gtk_theme='Catppuccin-Mocha'
+icon_theme='Zafiro'
+cursor_theme='Qogirr'
+
+# Dunst
+dunst_width='300'
+dunst_height='80'
+dunst_offset='20x20'
+dunst_origin='top-right'
+dunst_font='Iosevka Custom 10'
+dunst_border='2'
+dunst_separator='2'
